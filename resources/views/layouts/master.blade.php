f1213487561e0e8df91e94eae021f0e3e38a3474
fabianabarrionuevo <fabibarrionuevo1@gmail.com>
2020-04-26 19:03:36 -0300

    algunos cambios

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="_token" content="{{ csrf_token() }}">
 
-    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Bebas+Neue|Bitter|Montserrat&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css"
@@ -32,7 +32,7 @@
     @include('includes.footer')
 
     {{-- Script js jquery --}}
-    <script src="{{ asset("js/app.js") }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <script>
         // Jquery cart
